d03b11ffaf759a50456205b3233db98372b91c8a
Laaribi Ayoub <s2101948@ic2s112-11.univ-lemans.fr>
2025-01-29 09:12:15 +0100

    modif case

--- menujavafx/src/main/java/com/menu/Case.java
+++ menujavafx/src/main/java/com/menu/Case.java
@@ -24,24 +24,6 @@ public abstract class Case {
         return this.grille;
     }
 
-     /**
-     * Methode qui renvoie le nombre d'aretes autour du point
-     * @return int
-     */
-    public int getTraitVoisin(){
-        
-        int x = this.getCoordX();
-        int y = this.getCoordY();
-        int nbArete = 0;
-        
-        // Si les case sont egale a une arete
-
-        if(((Arete)this.getGrille().getCase(x,y-1)).getEtat() == EnumEtat.TRAIT) nbArete++;
-        if(((Arete)this.getGrille().getCase(x-1,y)).getEtat() == EnumEtat.TRAIT) nbArete++;
-        if(((Arete)this.getGrille().getCase(x,y+1)).getEtat() == EnumEtat.TRAIT) nbArete++;
-        if(((Arete)this.getGrille().getCase(x+1,y)).getEtat() == EnumEtat.TRAIT) nbArete++;
-        
-        return nbArete;
-    }
+     
 
 }
--- menujavafx/src/main/java/com/menu/Chiffre.java
+++ menujavafx/src/main/java/com/menu/Chiffre.java
@@ -39,6 +39,27 @@ public class Chiffre extends Case {
         return false;
     }
 
+
+    /**
+     * Methode qui renvoie le nombre d'aretes autour du point
+     * @return int
+     */
+    public int getTraitVoisin(){
+        
+        int x = this.getCoordX();
+        int y = this.getCoordY();
+        int nbArete = 0;
+        
+        // Si les case sont egale a une arete
+
+        if(((Arete)this.getGrille().getCase(x,y-1)).getEtat() == EnumEtat.TRAIT) nbArete++;
+        if(((Arete)this.getGrille().getCase(x-1,y)).getEtat() == EnumEtat.TRAIT) nbArete++;
+        if(((Arete)this.getGrille().getCase(x,y+1)).getEtat() == EnumEtat.TRAIT) nbArete++;
+        if(((Arete)this.getGrille().getCase(x+1,y)).getEtat() == EnumEtat.TRAIT) nbArete++;
+        
+        return nbArete;
+    }
+
     public String toString(){
         if(this.chiffre == -1) return "   ";
         else return " " + this.chiffre + " ";
