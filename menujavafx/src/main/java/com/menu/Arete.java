afd172f46bcf2dd819abcaa873239aa7e9cb6993
AyoubV11 <alaaribi36@icloud.com>
2025-02-01 21:14:29 +0100

    dev: fonction check,estValide,estAutoriseAposerArete a tester

--- menujavafx/src/main/java/com/menu/Arete.java
+++ menujavafx/src/main/java/com/menu/Arete.java
@@ -30,26 +30,45 @@ public class Arete extends Case {
 
 
     /**
-     * Cette methode permet de verifier si l'utilisateur a le droit de poser l'arete
+     * Cette methode permet de verifier si la case est valide
      * @return boolean
      */
-
-     
-    public boolean check(){
-       
-        int ligne = this.getLigne();
-        int colonne = this.getColonne();
-        
-        System.out.println("ligne : "  + ligne);
-        System.out.println("Colonne : " +colonne);
-        
-        if(this.getGrille().getCase(ligne-1, colonne).getTraitVoisin() < 2 && this.getGrille().getCase(ligne+1, colonne).getTraitVoisin() < 2){
+    public boolean estValide(int ligne, int colonne) {
+        int nbLignes = grille.getNbLignes();
+        int nbColonnes = grille.getNbColonnes();
+    
+        // Vérifier si la position est dans les limites de la grille
+        return (ligne >= 0 && ligne < nbLignes && colonne >= 0 && colonne < nbColonnes);
+    }
+    
+    /**
+     * Cette methode permet de verifier si les cases voisines ont un nombre de  voisins inferieur a 2
+     * @return boolean
+     */
+    public boolean estAutoriseAPoserArete(int ligne , int colonne){
+        Grille grille =  this.getGrille();
+        if(grille.getCase(ligne-1, colonne).getTraitVoisin() < 2 && grille.getCase(ligne+1, colonne).getTraitVoisin() < 2
+        && grille.getCase(ligne, colonne-1).getTraitVoisin() < 2 && grille.getCase(ligne, colonne+1).getTraitVoisin() < 2){
             return true;
         }
        return false;
-   
+    }
 
+
+    /**
+     * Cette methode permet de verifier si l'utilisateur a le droit de poser l'arete, si la case est valide et qu'elle respecte la conditions des voisins
+     * @return boolean
+     */
+    public boolean check(){
+       
+        int ligne = this.getLigne();
+        int colonne = this.getColonne();     
         
+        
+        if(this.estValide(ligne, colonne) && this.estAutoriseAPoserArete(ligne,colonne)){
+            return true;
+        }
+        return false;
     }
     
     public String toString(){
