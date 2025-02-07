ea71187cffb56f3c8d57fe9d24fde236726f99ff
LennyB972 <lenny.borry@gmail.com>
2025-02-07 11:03:40 +0100

    fin BoxFactory

--- menujavafx/src/main/java/com/menu/BoxFactory.java
+++ menujavafx/src/main/java/com/menu/BoxFactory.java
@@ -1,6 +1,13 @@
 package com.menu;
 
+import javafx.geometry.Insets;
 import javafx.geometry.Pos;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.Slider;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 
 public class BoxFactory {
@@ -15,7 +22,69 @@ public class BoxFactory {
         return menu;
     }
 
+    public static VBox createSettingsBox(Menu menu) {
+        VBox settingsBox = createStyledBox(220, 260);
+        
+        HBox autoCrossToggle = ButtonFactory.createToggleButton("Croix auto : ", "OFF", "ON");
+        Label volumeLabel = new Label("Volume : 50%");
+        volumeLabel.setFont(BalooFont.setBalooSized(18));
+        Slider volumeSlider = ButtonFactory.createVolumeSlider(volumeLabel);
+        
+        Button retourButton = ButtonFactory.createAnimatedButton("RETOUR");
+        retourButton.setPrefWidth(200);
+        retourButton.setOnAction(e -> menu.showMainMenu());
+        
+        settingsBox.getChildren().addAll(autoCrossToggle, volumeLabel, volumeSlider, retourButton);
+        return settingsBox;
+    }
 
-    
+    public static VBox createStyledBox(int width, int height) {
+        VBox box = new VBox(15);
+        box.setAlignment(Pos.CENTER);
+        box.setStyle("-fx-background-color: #D3D3D3; -fx-padding: 20; -fx-border-radius: 15; -fx-background-radius: 15;");
+        box.setPrefSize(width, height);
+        box.setMinWidth(width);
+        box.setMaxWidth(width);
+        box.setMinHeight(height);
+        box.setMaxHeight(height);
+        return box;
+    }
+
+    public static VBox createLevelBox(Menu menu,String pathCompleted, String pathUncompleted) {
+        VBox levelBox = createStyledBox(440, 270);
+        HBox starsBox = new HBox(30, createStar(pathCompleted), createStar(pathCompleted), createStar(pathUncompleted));
+        starsBox.setAlignment(Pos.CENTER);
+
+        HBox descriptionBox = new HBox(30,createDescription("Finir le niveau"),createDescription("Moins de 2 aides"),createDescription("Moins de 3min"));
+        descriptionBox.setAlignment(Pos.CENTER);
+        
+        Button retourButton = ButtonFactory.createAnimatedButton("RETOUR");
+        retourButton.setOnAction(e -> menu.showClassicMenu());
+        retourButton.setPrefWidth(200);
+        Button jouerButton = ButtonFactory.createAnimatedButton("JOUER");
+        jouerButton.setOnAction(e -> menu.showSettings());
+        jouerButton.setPrefWidth(200);
+        
+        HBox buttonBox = new HBox(30, retourButton, jouerButton);
+        buttonBox.setAlignment(Pos.BOTTOM_LEFT);
+        buttonBox.setPadding(new Insets(10,10,10,10));
+        
+        levelBox.getChildren().addAll(starsBox,descriptionBox, buttonBox);
+        return levelBox;
+    }
+
+     private static Label createDescription(String description){
+        Label starDescription = new Label(description);
+        starDescription.setFont(BalooFont.setBalooSized(16));
+        starDescription.setAlignment(Pos.CENTER);
+        return starDescription;
+    }
+
+    private static ImageView createStar(String imagePath) {
+        ImageView star = new ImageView(new Image(imagePath));
+        star.setFitWidth(100);
+        star.setFitHeight(100);
+        return star;
+    }
 
 }
--- menujavafx/src/main/java/com/menu/Menu.java
+++ menujavafx/src/main/java/com/menu/Menu.java
@@ -1,32 +1,27 @@
 package com.menu;
 
-import javafx.animation.ScaleTransition;
 import javafx.application.Application;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
-import javafx.scene.control.*;
 import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
 import javafx.scene.layout.*;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
-import javafx.util.Duration;
 
 public class Menu extends Application {
     private GridComponent gridComponent;
     private HBox content;
     private VBox menuBox;
-    private MenuBoxComponent menuBoxComponent;
 
     @Override
     public void start(Stage primaryStage) {
         TitleComponent titleComponent = new TitleComponent();
-        menuBoxComponent = new MenuBoxComponent(this);
+        MenuBoxComponent menuBoxComponent = new MenuBoxComponent(this);
         gridComponent = new GridComponent(this);
 
         StackPane root = setupBackground();
-        menuBox = setupMenuBox();
+        menuBox = BoxFactory.setupMenuBox(menuBoxComponent);
         content = new HBox(40, menuBox, gridComponent.getGridPane());
         content.setStyle("-fx-padding: 20;");
         content.setAlignment(Pos.CENTER);
@@ -86,78 +81,14 @@ public class Menu extends Application {
     public void showSettings() {
         gridComponent.clear();
         content.getChildren().clear();
-        content.getChildren().addAll(createSettingsBox(), gridComponent.getGridPane());
+        content.getChildren().addAll(BoxFactory.createSettingsBox(this), gridComponent.getGridPane());
     }
 
-    private VBox createSettingsBox() {
-        VBox settingsBox = createStyledBox(220, 260);
-        
-        HBox autoCrossToggle = ButtonFactory.createToggleButton("Croix auto : ", "OFF", "ON");
-        Label volumeLabel = new Label("Volume : 50%");
-        volumeLabel.setFont(BalooFont.setBalooSized(18));
-        Slider volumeSlider = ButtonFactory.createVolumeSlider(volumeLabel);
-        
-        Button retourButton = ButtonFactory.createAnimatedButton("RETOUR");
-        retourButton.setPrefWidth(200);
-        retourButton.setOnAction(e -> showMainMenu());
-        
-        settingsBox.getChildren().addAll(autoCrossToggle, volumeLabel, volumeSlider, retourButton);
-        return settingsBox;
-    }
-
-    private VBox createStyledBox(int width, int height) {
-        VBox box = new VBox(15);
-        box.setAlignment(Pos.CENTER);
-        box.setStyle("-fx-background-color: #D3D3D3; -fx-padding: 20; -fx-border-radius: 15; -fx-background-radius: 15;");
-        box.setPrefSize(width, height);
-        box.setMinWidth(width);
-        box.setMaxWidth(width);
-        box.setMinHeight(height);
-        box.setMaxHeight(height);
-        return box;
-    }
 
     public void showLevel() {
         gridComponent.clear();
         content.getChildren().clear();
-        content.getChildren().addAll(createLevelBox(), gridComponent.getGridPane());
-    }
-
-    private VBox createLevelBox() {
-        VBox levelBox = createStyledBox(440, 270);
-        HBox starsBox = new HBox(30, createStar("/star_completed.png"), createStar("/star_completed.png"), createStar("/star_uncompleted.png"));
-        starsBox.setAlignment(Pos.CENTER);
-
-        HBox descriptionBox = new HBox(30,createDescription("Finir le niveau"),createDescription("Moins de 2 aides"),createDescription("Moins de 3min"));
-        descriptionBox.setAlignment(Pos.CENTER);
-        
-        Button retourButton = ButtonFactory.createAnimatedButton("RETOUR");
-        retourButton.setOnAction(e -> showClassicMenu());
-        retourButton.setPrefWidth(200);
-        Button jouerButton = ButtonFactory.createAnimatedButton("JOUER");
-        jouerButton.setOnAction(e -> showSettings());
-        jouerButton.setPrefWidth(200);
-        
-        HBox buttonBox = new HBox(30, retourButton, jouerButton);
-        buttonBox.setAlignment(Pos.BOTTOM_LEFT);
-        buttonBox.setPadding(new Insets(10,10,10,10));
-        
-        levelBox.getChildren().addAll(starsBox,descriptionBox, buttonBox);
-        return levelBox;
-    }
-
-    private Label createDescription(String description){
-        Label starDescription = new Label(description);
-        starDescription.setFont(BalooFont.setBalooSized(16));
-        starDescription.setAlignment(Pos.CENTER);
-        return starDescription;
-    }
-
-    private ImageView createStar(String resource) {
-        ImageView star = new ImageView(new Image(getClass().getResource(resource).toExternalForm()));
-        star.setFitWidth(100);
-        star.setFitHeight(100);
-        return star;
+        content.getChildren().addAll(BoxFactory.createLevelBox(this,getClass().getResource("/star_completed.png").toExternalForm(),getClass().getResource("/star_uncompleted.png").toExternalForm()), gridComponent.getGridPane());
     }
 
     public void showMainMenu() {
