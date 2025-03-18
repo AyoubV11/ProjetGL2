cf731c64fabf531a8b2be2017d58d418e8a7b93d
Bougle Dimitri <s204348@ic2s122-17.univ-lemans.fr>
2025-03-18 13:59:14 +0100

    modif techniques

--- menujavafx/src/main/java/com/menu/TechniqueDeux3Diagonal.java
+++ menujavafx/src/main/java/com/menu/TechniqueDeux3Diagonal.java
@@ -19,22 +19,22 @@ public class TechniqueDeux3Diagonal implements Technique {
             Chiffre c = it.next();
             if(c.getChiffre() == 3) {
                 if(c.getChiffreHautGauche().getChiffre() == 3) {
-                    if(((Arete)g.getCase(c.getLigne()-1, c.getColonne())).getEtat() != EnumEtat.TRAIT || ((Arete)g.getCase(c.getLigne(), c.getColonne()-1)).getEtat() != EnumEtat.TRAIT) {
+                    if(((Arete)g.getCase(c.getLigne(), c.getColonne()+1)).getEtat() != EnumEtat.TRAIT || ((Arete)g.getCase(c.getLigne()+1, c.getColonne())).getEtat() != EnumEtat.TRAIT || ((Arete)g.getCase(c.getLigne()-3, c.getColonne()-2)).getEtat() != EnumEtat.TRAIT || ((Arete)g.getCase(c.getLigne()-2, c.getColonne()-3)).getEtat() != EnumEtat.TRAIT) {
                         return true;
                     }
                 }
                 else if(c.getChiffreHautDroit().getChiffre() == 3) {
-                    if(((Arete)g.getCase(c.getLigne()-1, c.getColonne())).getEtat() != EnumEtat.TRAIT || ((Arete)g.getCase(c.getLigne(), c.getColonne()+1)).getEtat() != EnumEtat.TRAIT) {
+                    if(((Arete)g.getCase(c.getLigne(), c.getColonne()-1)).getEtat() != EnumEtat.TRAIT || ((Arete)g.getCase(c.getLigne()+1, c.getColonne())).getEtat() != EnumEtat.TRAIT || ((Arete)g.getCase(c.getLigne()-3, c.getColonne()+2)).getEtat() != EnumEtat.TRAIT || ((Arete)g.getCase(c.getLigne()-2, c.getColonne()+3)).getEtat() != EnumEtat.TRAIT) {
                         return true;
                     }
                 }
                 else if(c.getChiffreBasDroit().getChiffre() == 3) {
-                    if(((Arete)g.getCase(c.getLigne()+1, c.getColonne())).getEtat() != EnumEtat.TRAIT || ((Arete)g.getCase(c.getLigne(), c.getColonne()+1)).getEtat() != EnumEtat.TRAIT) {
+                    if(((Arete)g.getCase(c.getLigne()-1, c.getColonne())).getEtat() != EnumEtat.TRAIT || ((Arete)g.getCase(c.getLigne(), c.getColonne()-1)).getEtat() != EnumEtat.TRAIT || ((Arete)g.getCase(c.getLigne()+3, c.getColonne()+2)).getEtat() != EnumEtat.TRAIT || ((Arete)g.getCase(c.getLigne()+2, c.getColonne()+3)).getEtat() != EnumEtat.TRAIT) {
                         return true;
                     }
                 }
                 else if(c.getChiffreBasGauche().getChiffre() == 3) {
-                    if(((Arete)g.getCase(c.getLigne()+1, c.getColonne())).getEtat() != EnumEtat.TRAIT || ((Arete)g.getCase(c.getLigne(), c.getColonne()-1)).getEtat() != EnumEtat.TRAIT) {
+                    if(((Arete)g.getCase(c.getLigne()-1, c.getColonne())).getEtat() != EnumEtat.TRAIT || ((Arete)g.getCase(c.getLigne(), c.getColonne()+1)).getEtat() != EnumEtat.TRAIT || ((Arete)g.getCase(c.getLigne()+3, c.getColonne()-2)).getEtat() != EnumEtat.TRAIT || ((Arete)g.getCase(c.getLigne()+2, c.getColonne()-3)).getEtat() != EnumEtat.TRAIT) {
                         return true;
                     }
                 }
