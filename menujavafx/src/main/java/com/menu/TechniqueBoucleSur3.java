4d1d88c60e3b8858ebb9946d10ab4cc65315f9af
Bougle Dimitri <s204348@ic2s122-01.univ-lemans.fr>
2025-03-19 11:54:32 +0100

    correction aide

---
+++ menujavafx/src/main/java/com/menu/TechniqueBoucleSur3.java
@@ -0,0 +1,23 @@
+/*package com.menu;
+
+import java.util.Iterator;
+
+public class TechniqueBoucleSur3 implements Technique{
+    private Grille g;
+
+    public TechniqueBoucleSur3(Grille g) {
+        this.g = g;
+    }
+
+    public void afficherAide() {
+        System.out.println("Technique de la boucle qui arrive sur un 3 applicable");
+    }
+
+    public boolean applicable(){
+        Iterator<Chiffre> it = g.iteratorChiffres();
+        while(it.hasNext()){
+            Chiffre c = it.next();
+        }
+    }
+
+}*/
