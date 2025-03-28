f7606d8d5ee4e122e830fc52db907d9720f5852a
Patrick Pastouret <pastouretpatrick@gmail.com>
2025-03-28 17:55:34 +0100

    correction tatonnement

--- menujavafx/src/main/java/com/menu/Arete.java
+++ menujavafx/src/main/java/com/menu/Arete.java
@@ -58,23 +58,18 @@ public class Arete extends Case {
 
     public void pushAction(EnumEtat nouvelleEtat){
         Action action = new Action(this.ligne, this.colonne, nouvelleEtat, etat);
-        this.grille.getPileUndo().push(action);
-        this.grille.getPileRedo().clear();
-        if(!libre){
-            this.grille.sauvegarderProgression();
-        }
-        else{
-            this.grille.sauvegarderProgressionLibre();
-        }
         if(this.grille.enModeTatonnement()){
             this.grille.getPileUndoTatonnement().push(action);
             this.grille.getPileRedoTatonnement().clear();
         }else{
             this.grille.getPileUndo().push(action);
             this.grille.getPileRedo().clear();
-            
         }
-        this.grille.sauvegarderProgression();
+        if(!libre) 
+            this.grille.sauvegarderProgressionLibre();
+        else 
+            this.grille.sauvegarderProgression();
+        
     }
 
     /**
