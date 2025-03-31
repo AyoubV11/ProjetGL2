702427689f031bf6e38eb7310ed86bfeab00be77
LennyB972 <lenny.borry@gmail.com>
2025-03-31 16:40:20 +0200

    tech avancee 2 maj ultime

--- menujavafx/src/main/java/com/menu/TechniqueAvancee2.java
+++ menujavafx/src/main/java/com/menu/TechniqueAvancee2.java
@@ -114,7 +114,7 @@ public class TechniqueAvancee2 implements Technique {
                     /*voisins droit et bas */
                     else if (((Arete)g.getCase(c.getLigne()+1, c.getColonne())).getEtat() == EnumEtat.TRAIT && 
                              ((Arete)g.getCase(c.getLigne(), c.getColonne()+1)).getEtat() == EnumEtat.TRAIT &&
-                             ((Arete)g.getCase(c.getLigne(), c.getColonne()-5)).getEtat() == EnumEtat.TRAIT) {
+                             ((Arete)g.getCase(c.getLigne(), c.getColonne()-5)).getEtat() != EnumEtat.TRAIT) {
                         
                         logger.trace("3 en [{},{}] avec traits à droite et en bas", c.getLigne(), c.getColonne());
                         /**vérifier si ça ne déborde pas */
@@ -172,7 +172,7 @@ public class TechniqueAvancee2 implements Technique {
                     /*voisins bas et gauche */
                     else if (((Arete)g.getCase(c.getLigne()+1, c.getColonne())).getEtat() == EnumEtat.TRAIT && 
                              ((Arete)g.getCase(c.getLigne(), c.getColonne()-1)).getEtat() == EnumEtat.TRAIT &&
-                             ((Arete)g.getCase(c.getLigne(), c.getColonne()+5)).getEtat() == EnumEtat.TRAIT) {
+                             ((Arete)g.getCase(c.getLigne(), c.getColonne()+5)).getEtat() != EnumEtat.TRAIT) {
                         
                         logger.trace("3 en [{},{}] avec traits en bas et à gauche", c.getLigne(), c.getColonne());
                         /**vérifier si ça ne déborde pas */
@@ -230,7 +230,7 @@ public class TechniqueAvancee2 implements Technique {
                     /*voisins gauche et haut */
                     else if (((Arete)g.getCase(c.getLigne()-1, c.getColonne())).getEtat() == EnumEtat.TRAIT && 
                              ((Arete)g.getCase(c.getLigne(), c.getColonne()-1)).getEtat() == EnumEtat.TRAIT &&
-                             ((Arete)g.getCase(c.getLigne()+5, c.getColonne())).getEtat() == EnumEtat.TRAIT) {
+                             ((Arete)g.getCase(c.getLigne()+5, c.getColonne())).getEtat() != EnumEtat.TRAIT) {
                         
                         logger.trace("3 en [{},{}] avec traits à gauche et en haut", c.getLigne(), c.getColonne());
                         /**vérifier si ça ne déborde pas */
